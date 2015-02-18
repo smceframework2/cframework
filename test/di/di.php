0722b2e64f95558188119c46e2acba86427b01c9
Samed Ceylan <imadige@gmail.com>
2015-02-18 13:42:16 +0200

    update

--- test/di/di.php
+++ test/di/di.php
@@ -5,20 +5,49 @@ class Di  extends PHPUnit_Framework_TestCase
 {
 	public function test1()
     {	
+
+    	//new di
     	$di=new Smce\Core\DI;
 
+    	//bind
 		$di->bind("foo",function(){
 
 		    return new Foo;
 
 		});
 
-
+		//resolve
 		$foo=$di->resolve("foo");
-		$foo2=Smce\Core\DI::resolve("foo");
 
+
+		//get
 		$this->assertTrue( $foo->get() );
-		$this->assertTrue( $foo2->get() );
+
+		//has
+		$this->assertTrue( Smce\Core\DI::has("foo") );
+
+		//getKeys
+		$this->assertTrue( is_array(Smce\Core\DI::getKeys()) );
+
+		//getAll
+		$this->assertTrue( is_array(Smce\Core\DI::getAll()) );
+
+		//getCount
+		$this->assertInternalType("int", Smce\Core\DI::getCount() );
+
+
+		//reset
+		if(Smce\Core\DI::reset())
+		{
+
+			$this->assertTrue(true);
+
+		}else{
+
+			$this->assertTrue(false);
+
+		}
+
     }
 }
 
